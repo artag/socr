385b233d57b5b451242300b4cde43f54df014a29
Artem Agafonov <agart@mail.ru>
2025-05-31 00:32:26 +0300

    Main. Add ScreenshotFromScreen, Ocr.
    Draft versions.

---
+++ src/Socr.Main/Ocr.cs
@@ -0,0 +1,18 @@
+﻿using CSharpFunctionalExtensions;
+using Socr.Domain;
+using Tesseract;
+
+namespace Socr.Main;
+
+internal sealed class Ocr
+{
+    public Task<Result<RecognizedText>> RecognizeText(Screenshot screenshot)
+    {
+        using var engine = new TesseractEngine("E:\\Temp\\1", "eng", EngineMode.LstmOnly);
+        using var img = Pix.LoadFromMemory(screenshot.Data);
+        using var recognizedPage = engine.Process(img);
+        var text = recognizedPage.GetText();
+        var recognizedText = new RecognizedText(text);
+        return Task.FromResult(Result.Success(recognizedText));
+    }
+}
---
+++ src/Socr.Main/ScreenshotFromScreen.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+using ImageMagick;
+using Socr.Domain;
+
+namespace Socr.Main;
+
+internal sealed class ScreenshotFromScreen
+{
+    public Task<Result<Screenshot>> Make(ScreenRegion screenRegion)
+    {
+        var x = screenRegion.X;
+        var y = screenRegion.Y;
+        var width = screenRegion.Width;
+        var height = screenRegion.Height;
+        var settings = new MagickReadSettings
+        {
+            ExtractArea = new MagickGeometry(x, y, width, height)
+        };
+
+        using var mImage = new ImageMagick.MagickImage("screenshot:", settings);
+        var data = mImage.ToByteArray(MagickFormat.Png);
+        var screenShot = new Screenshot(data);
+        return Task.FromResult(Result.Success(screenShot));
+    }
+}
