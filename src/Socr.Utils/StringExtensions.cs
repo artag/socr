6864b6fc8ab2d4e84b9e5bd2a238f13d9e7d9d2b
Artem Agafonov <agart@mail.ru>
2025-05-31 00:26:39 +0300

    Utils. Add StringExtensions.

---
+++ src/Socr.Utils/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace Socr.Utils;
+
+/// <summary>
+/// Extensions methods for <see cref="string"/>.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Indicates whether the specified string is null or an empty string ("").
+    /// </summary>
+    /// <param name="value">The string to test.</param>
+    /// <returns>
+    /// true if the value parameter is null or an empty string (""); otherwise, false.
+    /// </returns>
+    public static bool IsNullOrEmpty(this string? value) =>
+        string.IsNullOrEmpty(value);
+
+    /// <summary>
+    /// Indicates whether the specified string is NOT null or NOT an empty string ("").
+    /// </summary>
+    /// <param name="value">The string to test.</param>
+    /// <returns>
+    /// true if the value parameter is NOT null or NOT an empty string (""); otherwise, false.
+    /// </returns>
+    public static bool IsNotNullOrEmpty(this string? value) =>
+        !string.IsNullOrEmpty(value);
+
+    /// <summary>
+    /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+    /// </summary>
+    /// <param name="value">The string to test.</param>
+    /// <returns>
+    /// true if the value parameter is null or Empty, or if value consists exclusively of white-space characters.
+    /// </returns>
+    public static bool IsNullOrWhiteSpace(this string? value) =>
+        string.IsNullOrWhiteSpace(value);
+
+    /// <summary>
+    /// Indicates whether a specified string is NOT null, empty, or consists only of white-space characters.
+    /// </summary>
+    /// <param name="value">The string to test.</param>
+    /// <returns>
+    /// true if the value parameter is NOT null or Empty, or if value consists exclusively of white-space characters.
+    /// </returns>
+    public static bool IsNotNullOrWhiteSpace(this string? value) =>
+        !string.IsNullOrWhiteSpace(value);
+}
