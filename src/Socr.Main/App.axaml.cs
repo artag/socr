608d6135ec51dc3b3afb6cc763e4e3509084dca8
Artem Agafonov <agart@mail.ru>
2025-06-01 16:09:55 +0300

    App. Add draft logic implementations.

--- src/Socr.Main/App.axaml.cs
+++ src/Socr.Main/App.axaml.cs
@@ -1,8 +1,11 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input.Platform;
 using Avalonia.Markup.Xaml;
 using CSharpFunctionalExtensions;
 using Socr.Domain;
+using Tesseract;
 
 namespace Socr.Main;
 
@@ -22,28 +25,60 @@ internal sealed partial class App : Application
             var screenshot = new ScreenshotFromScreen();
             var ocr = new Ocr();
 
+            var mainWindow = new MainWindow();
+            var topLevel = TopLevel.GetTopLevel(mainWindow);
+            var clipboard = new Clipboard(topLevel!.Clipboard!);
+            var display = new DisplayAdapter(mainWindow);
             var mainScenario = new MainScenario(
                 screenshot.Make,
                 ocr.RecognizeText,
-                SetRecognizedTextToClipboard,
-                DisplayMessage);
+                clipboard.SetRecognizedText,
+                display.DisplayMessage);
 
-            desktop.MainWindow = new MainWindow()
-            {
-                MainScenario = mainScenario
-            };
+            mainWindow.MainScenario = mainScenario;
+            desktop.MainWindow = mainWindow;
         }
 
         base.OnFrameworkInitializationCompleted();
     }
+}
 
-    private Task<Result> SetRecognizedTextToClipboard(RecognizedText recognizedText)
+internal class Clipboard
+{
+    private readonly IClipboard _clipboard;
+
+    public Clipboard(IClipboard clipboard)
     {
-        throw new NotImplementedException();
+        _clipboard = clipboard ?? throw new ArgumentNullException(nameof(clipboard));
     }
 
-    private Result DisplayMessage(Message message)
+    public Task<Result> SetRecognizedText(RecognizedText recognizedText)
     {
-        throw new NotImplementedException();
+        return Result.Try(
+        async () =>
+        {
+            await _clipboard
+                .SetTextAsync(recognizedText.Value)
+                .ConfigureAwait(false);
+        });
+    }
+}
+
+internal class DisplayAdapter
+{
+    private readonly MainWindow _mainWindow;
+
+    public DisplayAdapter(MainWindow mainWindow)
+    {
+        _mainWindow = mainWindow ?? throw new ArgumentNullException(nameof(mainWindow));
+    }
+
+    public Result DisplayMessage(Message message)
+    {
+        return Result.Try(
+        () =>
+        {
+            _mainWindow.SetStatus(message.Value);
+        });
     }
 }
